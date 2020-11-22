8653b2ace3b716cba696188941914e95cf59990e
basahota <techbasanta757@gmail.com>
2020-11-22 23:17:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Alert from 'react-bootstrap/Alert'
+import Container from 'react-bootstrap/Container'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+
+      <Container className='mt-1' fluid>
+        <Alert className='text' variant={'primary'}>Primary Alert</Alert>
+        <Alert className='text' variant={'success'}>Success Alert</Alert>
+        <Alert className='text' variant={'warning'}>Warning Alert</Alert>
+        <Alert className='text' variant={'danger '}>Danger Alert</Alert>
+        <Alert className='text' variant={'info '}>Info Alert</Alert>
+
+        {/* <Alert>
+          <AlertHeading>Rather than line all alert manually we can iterate and use it</AlertHeading>
+        </Alert> */}
+        {/* 
+Rather than line all alert manually we can iterate and use it */}
+        {
+          ['primary', 'success', 'danger', 'info', 'warning', 'light', 'dark'].map((element, index) => (
+            <Alert className='text' variant={element}>Primary Alert</Alert>
+          ))
+        }
+      </Container>
+    );
+  }
 }
-
-export default App;
